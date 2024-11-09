d9d2899d3eea4f26a499bf06f57f88f3c0feb5c6
Vaibhav-afk <Vaibhav.Kumar@anyonehome.com>
2024-11-10 01:35:59 +0530

    basic setup done

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Vaibhav");
+});
+
+
+app.listen(5000, () => {
+  console.log("Server is running on 5000 port");
+});
